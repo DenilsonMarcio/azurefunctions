c6494761b8c008f5c9df87667fb04debc7cb47e3
Denilson Márcio <moppahtech@gmail.com>
2021-01-06 16:07:38 -0300

    Update Funcion 4

--- src/main/java/com/function/Function_4.java
+++ src/main/java/com/function/Function_4.java
@@ -1,32 +1,61 @@
 package com.function;
 
-import java.util.*;
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import org.json.JSONObject;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function_4 {
     /**
-     * This function listens at endpoint "/api/Function_4". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/Function_4
-     * 2. curl {your host}/api/Function_4?name=HTTP%20Query
+     * This function listens at endpoint "/api/Function_4". Two ways to invoke it
+     * using "curl" command in bash: 1. curl -d "HTTP Body" {your
+     * host}/api/Function_4 2. curl {your host}/api/Function_4?name=HTTP%20Query
      */
-    @FunctionName("Function_4")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+    @FunctionName("functionvalidationheader")
+    public HttpResponseMessage runPathNames(
+            @HttpTrigger(name = "req", methods = {
+                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+        String header = request.getHeaders().get("useridentifier");
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        try {
+
+            if (header.isEmpty() || header.isBlank() || !header.equals("azurefunctions")) {
+                return request.createResponseBuilder(HttpStatus.UNAUTHORIZED).build();
+            } else {
+                String jsonName = "";
+
+                try {
+
+                    JSONObject json = new JSONObject(request.getBody());
+                    jsonName = json.getString("nome");
+
+                    if (jsonName.equalsIgnoreCase("joão") || jsonName.equalsIgnoreCase("josé")
+                            || jsonName.equalsIgnoreCase("joaquim")) {
+                        return request.createResponseBuilder(HttpStatus.OK).body(jsonName).build();
+                    } else {
+                        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(jsonName).build();
+
+                    }
+                } catch (Exception e) {
+                    return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(jsonName).build();
+                }
+
+            }
+
+        } catch (Exception e) {
+
+            return request.createResponseBuilder(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
